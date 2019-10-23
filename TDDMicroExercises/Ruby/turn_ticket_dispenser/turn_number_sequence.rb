5e325ae351c4ca6cba345d1c92bb3edf2cb8140d
Paul van der Slot <paul.vd.slot@rigd-loxia.nl>
2019-10-23 14:47:11 +0200

    Delete all other than Java and English

--- TDDMicroExercises/Ruby/turn_ticket_dispenser/turn_number_sequence.rb
+++
@@ -1,7 +0,0 @@
-class TurnNumberSequence
-  @turn_number = 0
-
-  def self.get_next_turn_number
-    @turn_number += 1
-  end
-end
--- TDDMicroExercises/Ruby/turn_ticket_dispenser/turn_ticket.rb
+++
@@ -1,7 +0,0 @@
-class TurnTicket
-  attr_reader :turn_number
-
-  def initialize(turn_number)
-    @turn_number = turn_number
-  end
-end
