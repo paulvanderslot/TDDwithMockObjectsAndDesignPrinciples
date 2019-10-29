7e304d7ecc9047fe317e5564b3d827b7a15d4ca7
Paul van der Slot <paul.vd.slot@rigd-loxia.nl>
2019-10-29 14:55:09 +0100

    split reading lines and converting to html

--- TDDMicroExercises/Java/TextConverter/src/main/java/tddmicroexercises/textconvertor/UnicodeFileToHtmlTextConverter.java
+++ TDDMicroExercises/Java/TextConverter/src/main/java/tddmicroexercises/textconvertor/UnicodeFileToHtmlTextConverter.java
@@ -1,35 +1,43 @@
 package tddmicroexercises.textconvertor;
 
-import org.apache.commons.lang3.StringEscapeUtils;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 
-public class UnicodeFileToHtmlTextConverter
-{
+import org.apache.commons.lang3.StringEscapeUtils;
+
+public class UnicodeFileToHtmlTextConverter {
+    private static final String LINE_BREAK = "<br />";
     private String fullFilenameWithPath;
 
-    public UnicodeFileToHtmlTextConverter(String fullFilenameWithPath)
-    {
+    public UnicodeFileToHtmlTextConverter(String fullFilenameWithPath) {
         this.fullFilenameWithPath = fullFilenameWithPath;
     }
 
-    public String convertToHtml() throws IOException
-    {
-        try (BufferedReader reader = new BufferedReader(new FileReader(fullFilenameWithPath)))
-        {
-
-            String line = reader.readLine();
-            String html = "";
-            while (line != null)
-            {
-                html += StringEscapeUtils.escapeHtml4(line);
-                html += "<br />";
-                line = reader.readLine();
-            }
-            return html;
-
+    public String convertToHtml() throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fullFilenameWithPath))) {
+            StringBuilder htmlStringBuilder = new StringBuilder();
+            readLines(reader).forEach(line -> htmlStringBuilder.append(convertToHtml(line)));
+            return htmlStringBuilder.toString();
         }
+    }
+
+    private Collection<String> readLines(BufferedReader reader) throws IOException {
+        Collection<String> lines = new ArrayList<>();
+        String line = reader.readLine();
+        while (line != null) {
+            lines.add(line);
+            line = reader.readLine();
+        }
+        return lines;
+    }
+
+    private String convertToHtml(String line) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(StringEscapeUtils.escapeHtml4(line));
+        builder.append(LINE_BREAK);
+        return builder.toString();
     }
 }
